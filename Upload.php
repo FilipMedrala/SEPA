3955778983195dedf27192d4841f8eb2c77f6568
ryanmc20 <80500728+ryanmc20@users.noreply.github.com>
2021-08-15 15:08:12 +1000

    Create Upload.php

---
+++ Upload.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!-- This page contains the upload function -->
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="keywords" content="upload, fuzzing" />
+  <meta name="description" content="Page for uploading">
+  <meta name="author" content="Group 23" />
+ <!-- <link href= "styles/style.css" rel="stylesheet" /> -->
+ <!-- <script src="scripts/apply.js"></script> -->
+
+	<title> Upload </title>
+</head>
+<body id = "uploadPage">
+  <section id = "top">
+  <?php
+    include ("header.inc");
+    include ("navbar.inc");
+  ?>
+  </section>
+  <article>
+
+  <form action="upload.php" method="post" enctype="multipart/form-data">
+      Select file to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload File" name="submit">
+  </form>
+  </article>
+
+  <section id = "top">
+  <?php
+    include ("footer.inc");
+  ?>
+  </section>
+</body>
+</html>
