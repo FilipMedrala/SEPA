8b669e63d23b6738d1ef20542b70a8f5637d5ec3
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-08-28 11:19:27 +1000

    Add auto installation script
    Add the auto install script that will prepare the fuzzing server environment

---
+++ installAflPrereq.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+echo "SEPA P23 Auto-install Script"
+
+install2104() {
+	echo "Detected Ubuntu 21.04!"
+	echo "Updating current packages..."
+	sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+	echo "Installing prerequitsites for AFL/AFL++..."
+	sudo apt-get install -y build-essential python3-dev automake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools
+	sudo apt-get install -y lld-12 llvm-12 llvm-12-dev clang-12
+	sudo apt-get install -y gcc-10-plugin-dev libstdc++-10-dev
+
+	echo "Cloning AFL++ git repo..."
+	git clone https://github.com/AFLplusplus/AFLplusplus && cd AFLplusplus
+
+	echo "Compiling AFL++ with default settings..."
+	make distrib
+
+	echo "Installing AFL++"
+	sudo make install
+	cd ../
+
+	echo "Cloning AFL Fuzzing test program fuzzgoat..."
+	cd $HOME/Documents
+	git clone https://github.com/fuzzstati0n/fuzzgoat && cd fuzzgoat
+	echo "Changing fuzzgoat compiler to LTO"
+	export CC=afl-clang-lto++
+
+	echo "Compiling fuzzgoat"
+	sudo make
+	cd ../
+
+	echo "Installing web environment..."
+	sudo apt-get install apache2 php libapache2-mod-php
+
+	echo "Done!"
+}
+
+install2104() {
+	echo "Detected Ubuntu 20.04 LTS!"
+	echo "Updating current packages..."
+	sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+	echo "Installing prerequitsites for AFL/AFL++..."
+	sudo apt-get install -y build-essential python3-dev automake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools
+	sudo apt-get install -y lld-10 llvm-10 llvm-10-dev clang-10
+	sudo apt-get install -y gcc-9-plugin-dev libstdc++-9-dev
+
+	echo "Cloning AFL++ git repo..."
+	git clone https://github.com/AFLplusplus/AFLplusplus && cd AFLplusplus
+
+	echo "Compiling AFL++ with default settings..."
+	make distrib
+
+	echo "Installing AFL++"
+	sudo make install
+	cd ../
+
+	echo "Cloning AFL Fuzzing test program fuzzgoat..."
+	cd $HOME/Documents
+	git clone https://github.com/fuzzstati0n/fuzzgoat && cd fuzzgoat
+	echo "Changing fuzzgoat compiler to LTO"
+	export CC=afl-clang-lto++
+
+	echo "Compiling fuzzgoat"
+	sudo make
+	cd ../
+
+	echo "Installing web environment..."
+	sudo apt-get install apache2 php libapache2-mod-php
+
+	echo "Done!"
+}
+
+if [[ $(lsb_release -rs) == "21.04" ]]
+then
+	install2104()
+elif [[ $(lsb_release -rs) == "20.04" ]]
+then
+	install2004()
+else
+	echo "Script is only compatibile with Ubuntu 20.04 LTS or 21.04"
+fi
