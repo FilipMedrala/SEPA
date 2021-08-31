62871d21e7dfaa0888ed9bfc0050e2e0b9f8c908
ryanmc20 <80500728+ryanmc20@users.noreply.github.com>
2021-08-31 13:08:21 +1000

    How To page

--- Test/index.php
+++ Test/index.php
@@ -5,6 +5,21 @@
 <?php include 'ui/ui-header-index.inc';?>
 <body class="d-flex flex-column h-100">
 <?php include 'ui/ui-nav-static.inc';?>
+
+<div class="heading-page header-text">
+  <section class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="text-content">
+            <h4>Welcome to the User Dashboard</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
 <?php include 'ui/ui-body-index.inc';?>
 <?php include 'ui/ui-footer.inc';?>
 </body>
