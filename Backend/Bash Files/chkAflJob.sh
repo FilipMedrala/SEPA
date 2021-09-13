dbb19b5950029f9dbfbb26bdf20fc0ca21bd7af5
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-09-13 10:22:10 +1000

    Add process monitoring script
    Add the process monitoring script that allows periodic checks on a running job.

---
+++ Backend/Bash Files/chkAflJob.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## Active AFL++ job directory
+JOB_DIR_ACTIVE=$1
+## Specify summary or detail mode
+JOB_DETAILS=$2
+
+## Check if paths are empty
+if [ -z "$JOB_DIR_ACTIVE" || -z "$JOB_DETAILS" ]
+then
+  echo "ERROR: One or more variables supplied is empty"
+else
+  ## say nothing
+fi
+
+## Replace the JOB_DETAILS variable with correct value for afl-whatsup
+if [ $JOB_DETAILS == "summary" ]
+then
+  JOB_DETAILS="-s"
+elif [ $JOB_DETAILS == "detail" ]
+then
+  JOB_DETAILS=""
+else
+  echo "ERROR: Invalid job details option specified!"
+fi
+
+## Save the result to a file
+## for the frontend web application to pull and display
+saveToFile() {
+  afl-whatsup $JOB_DETAILS $JOB_DIR_ACTIVE | tee "$JOB_DIR_ACTIVE/afl-whatsup-latest.txt"
+}
