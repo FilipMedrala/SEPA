46e4286f1e8a994c74bef7f61e112a3c0d575766
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-09-13 10:57:03 +1000

    Fixes for ssl script
    Make modular.
    Add possibility for bootstrapping real certificates.

--- Backend/Bash Files/createSslCert.sh
+++ Backend/Bash Files/createSslCert.sh
@@ -3,15 +3,28 @@
 CERT_TYPE=$1
 CERT_OUT_DIR=$2
 
-## Certificate variables
-## Default key algo is elliptic cure P256
-CERT_ALGO="secp256r1"
-## Default duration is 1 year
-CERT_DURATION="-days 365"
-## Default subject is:
-CERT_SUBJECT='-subj "/C=AU/ST=Victoria/L=Hawthorn/O=SUT/CN=Project 23"'
+if [ $CERT_TYPE == "self" ]
+then
+  createSelfSigned
+elif [ $CERT_TYPE == "real" ]
+then
+  echo "Creating real SSL certificates is currently unsupported"
+  echo "Functionality may be added via LE later"
+  #createRealSigned
+else
+  echo "ERROR: Certificate type variable is unrecognised!"
 
-## Generate private key
-openssl ecparam -genkey -name $CERT_ALGO -noout -out $CERT_OUT_DIR/aflfuzz-web-key.pem
-## Cenerate x509 certificate
-openssl req -x509 -key aflfuzz-web-key.pem -out $CERT_OUT_DIR/aflfuzz-web-cert.pem $CERT_DURATION $CERT_SUBJECT
+createSelfSigned() {
+  ## Certificate variables
+  ## Default key algo is elliptic cure P256
+  CERT_ALGO="secp256r1"
+  ## Default duration is 1 year
+  CERT_DURATION="-days 365"
+  ## Default subject is:
+  CERT_SUBJECT='-subj "/C=AU/ST=Victoria/L=Hawthorn/O=SUT/CN=Project 23"'
+
+  ## Generate private key
+  openssl ecparam -genkey -name $CERT_ALGO -noout -out $CERT_OUT_DIR/aflfuzz-web-key.pem
+  ## Cenerate x509 certificate
+  openssl req -x509 -key aflfuzz-web-key.pem -out $CERT_OUT_DIR/aflfuzz-web-cert.pem $CERT_DURATION $CERT_SUBJECT
+}
