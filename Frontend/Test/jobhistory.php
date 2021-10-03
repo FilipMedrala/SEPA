1f0edef0c8b826e58fd4077727d063fa45157717
ryanmc20 <80500728+ryanmc20@users.noreply.github.com>
2021-10-03 16:38:08 +1100

    Update jobhistory.php

--- Frontend/Test/jobhistory.php
+++ Frontend/Test/jobhistory.php
@@ -5,7 +5,59 @@
 <?php include 'ui/ui-header-index.inc';?>
 <body class="d-flex flex-column h-100">
 <?php include 'ui/ui-nav-static.inc';?>
-<?php include 'ui/ui-body-jobhistory.inc';?>
+
+<div class="heading-page header-text">
+  <section class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="text-content">
+            <h4>How to use</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+<div>
+
+<p>Previous Items Fuzzed</p>
+<table>
+ <tr>
+   <th>Job 1</th>
+   <td>Test</td>
+   <td>5</td>
+ </tr>
+ <tr>
+   <th>Job 2</th>
+   <td>Test</td>
+   <td>5</td>
+ </tr>
+ <tr>
+   <th>Job 3</th>
+   <td>Test</td>
+   <td>14</td>
+ </tr>
+</table>
+</div>
+
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #D6EEEE;
+}
+</style>
+
+
 <?php include 'ui/ui-footer.inc';?>
 </body>
 </html>
