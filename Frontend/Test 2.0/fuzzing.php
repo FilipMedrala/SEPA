cb99fd0045f08ecd575b3afbf17b49a6a801d1ff
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-10-09 18:09:49 +1100

    add parameter name/id

--- Frontend/Test 2.0/fuzzing.php
+++ Frontend/Test 2.0/fuzzing.php
@@ -1,12 +1,31 @@
 <html>
 <body>
+<?php
+Application name: echo $_POST["appname"]; <br>
 
-Application name: <?php echo $_POST["appname"]; ?><br>
+## Get Parameters
+$P1 = ($_POST['userid']);
+$P2 = ($_POST['jobid']);
+$P3 = ($_POST['jobtype']);
+if (isset($_POST['param1'])) {
+  $P4 = ($_POST['param1'])
+};
+if (isset($_POST['param2'])) {
+  $P4 .= ($_POST['param2'])
+};
+if (isset($_POST['param3'])) {
+  $P4 .= ($_POST['param3'])
+};
+if (isset($_POST['param4'])) {
+  $P4 .= ($_POST['param4'])
+};
+if (isset($_POST['param5'])) {
+  $P4 .= ($_POST['param5'])
+};
 
 
 
 
-
-
+?>
 </body>
 </html>
