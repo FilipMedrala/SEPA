30edafb027ef0b8d6d80dbc95e22c8a25dba2d30
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-10-09 18:20:43 +1100

    Update fuzzing.php

--- Frontend/Test 2.0/fuzzing.php
+++ Frontend/Test 2.0/fuzzing.php
@@ -6,22 +6,12 @@ Application name: echo $_POST["appname"]; <br>
 ## Get Parameters
 $P1 = ($_POST['userid']);
 $P2 = ($_POST['jobid']);
-$P3 = ($_POST['jobtype']);
-if (isset($_POST['param1'])) {
-  $P4 = ($_POST['param1'])
-};
-if (isset($_POST['param2'])) {
-  $P4 .= ($_POST['param2'])
-};
-if (isset($_POST['param3'])) {
-  $P4 .= ($_POST['param3'])
-};
-if (isset($_POST['param4'])) {
-  $P4 .= ($_POST['param4'])
-};
-if (isset($_POST['param5'])) {
-  $P4 .= ($_POST['param5'])
-};
+$P3 = ($_POST['appname']);
+$P4 = ($_POST['jobtype']);
+$P5 = ($_POST['fastcalc']);
+$P6 = ($_POST['statsd']);
+
+echo "<p>$P1 $P2 $P3 $P4 $P5 $P6</p>"
 
 
 
