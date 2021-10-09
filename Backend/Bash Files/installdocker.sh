faa3a747f229d6394bb9b27cd4f376cdd995f105
Allan Ou <102311652@student.swin.edu.au>
2021-10-09 14:11:28 +1100

    Updating docker files

--- Backend/Bash Files/installdocker.sh
+++ Backend/Bash Files/installdocker.sh
@@ -2,7 +2,7 @@
 
 ## Update packages and dependencies
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg software-properties-common 
+sudo apt install -y apt-transport-https ca-certificates curl gnupg software-properties-common 
 
 ## Add GPG key for Docker repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -16,7 +16,7 @@ echo \
 sudo apt update
 
 ## Install Docker
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 ## Allow Docker commands to be run as a non-root user
 sudo usermod -aG docker ${USER}
---
+++ Backend/Bash Files/rundocker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# User ID
+uuid=$1
+# Job ID
+jid=$2
+# Target
+target=$3
+
+if [ -d "/home/sepadmin/Documents/afl/$uuid/$jid" ]
+then
+	docker run --name afl-$jid --network="host" -tid -v /home/sepadmin/Documents/afl/$uuid/$jid:/src --env-file /home/sepadmin/Documents/docker-build/.env-afl aflplusplus/aflplusplus
+	docker exec -tid afl-$jid afl-fuzz -D -i /src/in -o /src/out -- /src/source/$target @@
+else
+	echo "/home/sepadmin/Documents/afl/$uuid/$jid does not exist."
+fi
+
+
+##### TEST CODE ONLY
+#!/bin/bash
+
+# User ID
+uuid=$1
+# Job ID
+jid=$2
+
+if [ -d "/home/sepadmin/Documents/p23-directories/afl/$uuid/$jid" ]
+then
+	docker run --name afl-$jid --network="host" -tid -v /home/sepadmin/Documents/p23-directories/afl/$uuid/$jid:/src --env-file /home/sepadmin/Documents/docker-build/.env-afl aflplusplus/aflplusplus
+	docker exec -tid afl-$jid afl-fuzz -D -i /src/in -o /src/out -- /src/source @@
+else
+	echo "/home/sepadmin/Documents/p23-directories/afl/$uuid/$jid does not exist."
+fi
