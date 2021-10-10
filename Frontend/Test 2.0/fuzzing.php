cca056e0358b8123df5696320b1fafc9d54d2706
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-10-10 15:50:16 +1100

    Fix upload bugs

--- Frontend/Test 2.0/fuzzing.php
+++ Frontend/Test 2.0/fuzzing.php
@@ -10,26 +10,27 @@ $P3 = ($_POST['appname']);
 $P4 = ($_POST['jobtype']);
 $P5 = ($_POST['fastcal']);
 $P6 = ($_POST['statsd']);
+$P7 = ($_POST['compiler']);
 
-echo "<p>$P1 - $P2 - $P3 - $P4 - $P5 - $P6</p>";
+echo "<p>Variables dump: $P1, $P2, $P3, $P4, $P5, $P6</p>";
 
 $upload_dir = "/var/www/html/aflfuzzerweb/files/uploadtmp/";
-$upload_file = $upload_usr_dir . basename($_FILES["usrFile"]["name"]);
+$upload_file = $upload_dir . basename($_FILES["chooseFile"]["name"]);
 
-if (move_uploaded_file($_FILES["usrFile"]["tmp_name"], $upload_file)) {
+if (move_uploaded_file($_FILES["chooseFile"]["tmp_name"], $upload_file)) {
   $upload_is_ok = 1;
+  $filename=$_FILES['chooseFile']['name'];
+  echo 'your filename is: ' . $filename;
+  echo '<p>Binary uploaded successfully!</p>';
+  echo 'website should work';
 }
 else {
   $upload_is_ok = 0;
+  $filename=$_FILES['chooseFile']['name'];
+  echo 'your filename is: ' . $filename;
+  echo '<p>Binary not uploaded successfully, please check logs!</p>';
+  echo 'website is cooked';
 }
-
-#if ($upload_is_ok === 1) {
-#  echo '<p>Binary uploaded successfully!</p>';
-#  echo 'website should work';
-#else {
-#  echo '<p>Binary not uploaded successfully, please check logs!</p>';
-#  echo 'website is cooked';
-#}
 
 
 
