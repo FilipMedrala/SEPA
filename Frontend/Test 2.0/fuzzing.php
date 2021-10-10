7d3bde053d0580c940d44be3554a20473f4a8e4a
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-10-10 15:20:56 +1100

    Minor fixes

--- Frontend/Test 2.0/fuzzing.php
+++ Frontend/Test 2.0/fuzzing.php
@@ -1,28 +1,28 @@
 <html>
 <body>
 <?php
-echo "Application name: " ($_POST['appname']);
+echo "Application name: " . ($_POST['appname']);
 
 ## Get Parameters
 $P1 = ($_POST['userid']);
 $P2 = ($_POST['jobid']);
 $P3 = ($_POST['appname']);
 $P4 = ($_POST['jobtype']);
-$P5 = ($_POST['fastcalc']);
+$P5 = ($_POST['fastcal']);
 $P6 = ($_POST['statsd']);
 
 echo "<p>$P1 - $P2 - $P3 - $P4 - $P5 - $P6</p>";
 
 $upload_dir = "/var/www/html/aflfuzzerweb/files/uploadtmp/";
 $upload_file = $upload_usr_dir . basename($_FILES["usrFile"]["name"]);
-## move from temporary directory to acutal user directory
+
 if (move_uploaded_file($_FILES["usrFile"]["tmp_name"], $upload_file)) {
   $upload_is_ok = 1;
 }
 else {
   $upload_is_ok = 0;
 }
-## check upload status and print message
+
 if ($upload_is_ok === 1) {
   echo '<p>Binary uploaded successfully!</p>';
   echo "website should work";
