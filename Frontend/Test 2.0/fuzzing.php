8accb9d72230a654eae03407b149b34aeaaf6a87
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-10-10 16:20:16 +1100

    Fix more upload bugs

--- Frontend/Test 2.0/fuzzing.php
+++ Frontend/Test 2.0/fuzzing.php
@@ -1,7 +1,5 @@
-<html>
-<body>
 <?php
-echo "Application name: " . ($_POST['appname']);
+#echo "Application name: " . ($_POST['appname']);
 
 ## Get Parameters
 $P1 = ($_POST['userid']);
@@ -12,25 +10,43 @@ $P5 = ($_POST['fastcal']);
 $P6 = ($_POST['statsd']);
 $P7 = ($_POST['compiler']);
 
-echo "<p>Variables dump: $P1, $P2, $P3, $P4, $P5, $P6</p>";
+#echo "<p>Variables dump: $P1, $P2, $P3, $P4, $P5, $P6, $P7</p>";
 
 $upload_dir = "/var/www/html/aflfuzzerweb/files/uploadtmp/";
-$upload_file = $upload_dir . basename($_FILES["chooseFile"]["name"]);
+$upload_file1 = $upload_dir . basename($_FILES["chooseFile"]["name"]);
+$upload_file2 = $upload_dir . basename($_FILES["chooseFile2"]["name"]);
 
-if (move_uploaded_file($_FILES["chooseFile"]["tmp_name"], $upload_file)) {
+if (move_uploaded_file($_FILES["chooseFile"]["tmp_name"], $upload_file1)) {
   $upload_is_ok = 1;
-  $filename=$_FILES['chooseFile']['name'];
-  echo 'your filename is: ' . $filename;
-  echo '<p>Binary uploaded successfully!</p>';
-  echo 'website should work';
+  $filename1=$_FILES['chooseFile']['name'];
+#  echo 'your filename is: ' . $filename;
+#  echo '<p>First binary uploaded successfully!</p>';
+#  echo 'website should work';
+  exec(/bin/bash /home/sepadmin/Documents/AFLscripts/startTheJobs.sh $P1 $P2 $P4 $P5 $P6 $filename1)
 }
 else {
   $upload_is_ok = 0;
-  $filename=$_FILES['chooseFile']['name'];
-  echo 'your filename is: ' . $filename;
-  echo '<p>Binary not uploaded successfully, please check logs!</p>';
-  echo 'website is cooked';
+  $filename1=$_FILES['chooseFile']['name'];
+#  echo 'your first filename is: ' . $filename;
+#  echo '<p>First binary not uploaded successfully, please check logs!</p>';
+#  echo 'website is cooked';
 }
+if (move_uploaded_file($_FILES["chooseFile2"]["tmp_name"], $upload_file2)) {
+  $upload_is_ok = 1;
+  $filename2=$_FILES['chooseFile2']['name'];
+#  echo 'your second filename is: ' . $filename;
+#  echo '<p>Second binary uploaded successfully!</p>';
+#  echo 'website should work';
+  exec(/bin/bash /home/sepadmin/Documents/AFLscripts/startTheJobs.sh $P1 $P2 $P4 $P5 $P6 $filename2)
+}
+else {
+  $upload_is_ok = 0;
+  $filename2=$_FILES['chooseFile2']['name'];
+#  echo 'your second filename is: ' . $filename;
+#  echo '<p>Second binary not uploaded successfully, please check logs!</p>';
+#  echo 'website is cooked';
+}
+
 
 
 
