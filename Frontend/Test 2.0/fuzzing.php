c9dcde565d3303040028ab3d204e6aac78dd1a0f
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-10-10 21:51:15 +1100

    Comment functions more detailed

--- Frontend/Test 2.0/fuzzing.php
+++ Frontend/Test 2.0/fuzzing.php
@@ -10,12 +10,14 @@ function sanitise_input($data)
   $data = htmlspecialchars($data);
   return $data;
 }
+## Sanitise to legal string
 function sanitise_input_l2($data)
 {
   $data = sanitise_input($data);
   $data = filter_var($data, FILTER_SANITIZE_STRING);
   return $data;
 }
+## Sanitise to legal integer
 function sanitise_input_l3($data)
 {
   $data = sanitise_input($data);
@@ -42,47 +44,55 @@ $P5san = sanitise_input_l3($P5);
 $P6san = sanitise_input_l2($P6);
 $P7san = sanitise_input_l2($P7);
 
+### Debug Paramter
+## View raw POST values in sent array
 #echo "<p>Variables dump: $P1san, $P2san, $P3san, $P4san, $P5san, $P6san, $P7san</p>";
 
+### Upload File Parameters
+## Set temporary destination directory
 $upload_dir = "/var/www/html/aflfuzzerweb/files/uploadtmp/";
 $upload_file1 = $upload_dir . basename($_FILES["chooseFile"]["name"]);
 $upload_file2 = $upload_dir . basename($_FILES["chooseFile2"]["name"]);
-
+## Move uploaded files in field 1 to the temporary destination directory
 if (move_uploaded_file($_FILES["chooseFile"]["tmp_name"], $upload_file1)) {
   $upload1_is_ok = 1;
   $filename1=$_FILES['chooseFile']['name'];
 #  echo 'your filename is: ' . $filename;
 #  echo '<p>First binary uploaded successfully!</p>';
-#  echo 'website should work';
-#  exec(/bin/bash /home/sepadmin/Documents/AFLscripts/startTheJobs.sh $P1 $P2 $P4 $P5 $P6 $filename1)
 }
 else {
   $upload1_is_ok = 0;
   $filename1=$_FILES['chooseFile']['name'];
 #  echo 'your first filename is: ' . $filename;
 #  echo '<p>First binary not uploaded successfully, please check logs!</p>';
-#  echo 'website is cooked';
 }
+## Move uploaded files in field 2 to the temporary destination directory
 if (move_uploaded_file($_FILES["chooseFile2"]["tmp_name"], $upload_file2)) {
+  ## Set file2 upload status to "successful"
   $upload2_is_ok = 1;
   $filename2=$_FILES['chooseFile2']['name'];
 #  echo 'your second filename is: ' . $filename;
 #  echo '<p>Second binary uploaded successfully!</p>';
-#  echo 'website should work';
 }
 else {
+  ## Set file2 upload status to "failed"
   $upload2_is_ok = 0;
   $filename2=$_FILES['chooseFile2']['name'];
 #  echo 'your second filename is: ' . $filename;
 #  echo '<p>Second binary not uploaded successfully, please check logs!</p>';
-#  echo 'website is cooked';
 }
 
+
+### Main Code Execution Block
+## Only start the AFL job via shell script if both .zip files have uploaded successfully
 if ($upload1_is_ok === 1 && $upload2_is_ok === 1) {
-  ## Start fuzzing job
-  exec("/bin/bash /home/sepadmin/Documents/AFLscripts/startTheJobs.sh $P1san $P2san $P3san $P4san $P5san $P6san $P7san $filename1 $filename2", );
+  ## Start fuzzing job via exec pointed to the bootstrapper script
+#  exec("/bin/bash /home/sepadmin/Documents/AFLscripts/startTheJobs.sh $P1san $P2san $P3san $P4san $P5san $P6san $P7san $filename1 $filename2", );
+  ## Execute this script as another user to bypass some bash commands not working under www-data
+  exec("sudo su - sepadmin -c /home/sepadmin/Documents/AFLscripts/startTheJobs.sh $P1san $P2san $P3san $P4san $P5san $P6san $P7san $filename1 $filename2", );
 }
 else {
+  ## Display failure message
   echo "Failed to start the requested AFL job: $P2, as one or both of the file uploads weren't successful.";
 }
 
