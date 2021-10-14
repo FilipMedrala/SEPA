c47c3780e403dbbe26e46852f4fb87fb4ae5a87e
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-10-14 18:35:09 +1100

    add bootstrap container

--- Frontend/Test 2.0/jobhistory.php
+++ Frontend/Test 2.0/jobhistory.php
@@ -1,26 +1,19 @@
 <!DOCTYPE html>
 <html class="h-100">
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="Group 23">
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
-
   <title>AFL fuzzing platform - Job History</title>
-
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-
   <!-- Additional CSS Files -->
   <link rel="stylesheet" href="assets/css/fontawesome.css">
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="assets/css/owl.css">
-
 </head>
-
 <!-- ***** Preloader Start ***** -->
 <div id="preloader">
     <div class="jumper">
@@ -30,10 +23,7 @@
     </div>
 </div>
 <!-- ***** Preloader End ***** -->
-
-
 <body>
-
 <!-- Header -->
 <header class="">
   <nav class="navbar navbar-expand-lg">
@@ -69,7 +59,6 @@
     </div>
   </nav>
 </header>
-
 <!-- Banner Starts Here -->
 <div class="heading-page header-text">
   <section class="page-heading">
@@ -85,7 +74,7 @@
   </section>
 </div>
 
-
+<div class="container">
 <p>Previous Items Fuzzed</p>
 <div>
 <table>
@@ -95,11 +84,8 @@
    <th>Download</th>
  </tr>
 
-
-
 <?php
 session_start();
-
 
 require_once ("settings.php");
 $conn = @mysqli_connect(
@@ -161,6 +147,7 @@ for($i=0; $i < $count; $i++)
  ?>
 
 </table>
+</div>
 </div>
 
 
