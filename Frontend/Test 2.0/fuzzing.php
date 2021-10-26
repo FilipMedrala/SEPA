054389e2e28cf9d5dfa3be12946cd538b0496224
ozz306 <80500855+ozz306@users.noreply.github.com>
2021-10-26 18:10:22 +1100

    Add missing sql settings

--- Frontend/Test 2.0/fuzzing.php
+++ Frontend/Test 2.0/fuzzing.php
@@ -164,10 +164,24 @@ else {
   ";
 }
 
-$uid=($_COOKIE['userID']);
-$path="$uid/$p3san";
-$sql = "INSERT INTO files (uID, File, Adr) VALUES ('$uid', '$P3san', '$path')";
-$query = mysqli_query($conn, $sql);
+require_once ("settings.php");
+$conn = @mysqli_connect(
+$host,
+$user,
+$pwd,
+$sql_db
+);
+if (!$conn) {
+  echo "Mysql not connected ";
+  echo mysqli_connect_errno() . ":" . mysqli_connect_error();
+  exit;
+}
+else {
+  $uid=($_COOKIE['userID']);
+  $path="$uid/$p3san";
+  $sql = "INSERT INTO files (uID, File, Adr) VALUES ('$uid', '$P3san', '$path')";
+  $query = mysqli_query($conn, $sql);
+}
 
 ?>
 </div>
